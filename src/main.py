5b534c73de38e488fb56591d373a6ae80d04bebb
Jonasz Gawlik <mrbarman55@gmail.com>
2022-06-07 21:28:36 +0200

    small tweaks

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from jinja2 import pass_context
 import pandas as pd
 import json
 import plotly.express as px
@@ -15,11 +14,11 @@ app.layout = html.Div(children=[
         html.Div(children=[
             html.Div(children=[
                 html.H1(children=[
-                    'Kutas Kozła'
+                    'Praca Programisty w Polsce'
                 ])
             ]),
 
-        ]),  # TODO: Dodać switch'a na przełączanie pomiędzy erami, latami
+        ]),
         html.Nav(children=dcc.RadioItems(
             ['Porównanie Zarobków', 'Popularność Języków',
              'Oferty Zdalne', 'Ilość Ofert w Polsce',
@@ -63,7 +62,6 @@ def compare_income() -> go.Figure:
         x=porownanie_srednich_woj_inf['wojewodztwo'],
         y=porownanie_srednich_woj_inf['srednie_zarobki_wojewodztwo'],
         histfunc="sum",
-        # name used in legend and hover labels
         name='Średnia krajowa w danym województwie',
         marker_color='#fde65a',
         opacity=0.90
@@ -72,17 +70,16 @@ def compare_income() -> go.Figure:
         x=porownanie_srednich_woj_inf['wojewodztwo'],
         y=porownanie_srednich_woj_inf['srednie_zarobki_inf'],
         histfunc="sum",
-        # name used in legend and hover labels
         name='Średnie zarobki informatyków w danym województwie',
         marker_color='#551ebc',
         opacity=0.90
     ))
 
     fig.update_layout(
-        title_text='Porównanie średnich zarobków w danych województwach',  # title of plot
-        yaxis_title_text='Zarobki [zł]',  # yaxis label
-        bargap=0.2,  # gap between bars of adjacent location coordinates
-        bargroupgap=0.1,  # gap between bars of the same location coordinates
+        title_text='Porównanie średnich zarobków w danych województwach',
+        yaxis_title_text='Zarobki [zł]',
+        bargap=0.2,
+        bargroupgap=0.1,
         paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)',
         font_color='#cab656'
     )
@@ -104,9 +101,12 @@ def compare_languages_pop() -> go.Figure:
 def compare_offers_remote() -> go.Figure:
     df_jezyki = pd.read_csv("../data/zbior_jezyki_zdalnie.csv")
     df_jezyki["ilosc_ofert"] = df_jezyki.zdalnieT + df_jezyki.zdalnieN
-    labels = ["Zdalnie", "Stacjonarnie"]
+    labels = ["Praca Zdalna", "Praca Stacjonarna"]
     values = [df_jezyki.sum()["zdalnieT"], df_jezyki.sum()["zdalnieN"]]
-    return go.Figure(data=[go.Pie(labels=labels, values=values, marker_colors=['#fde65a', '#551ebc'])], layout=go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656'))
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, marker_colors=['#fde65a', '#551ebc'])], layout=go.Layout(
+        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656'))
+    fig.update_traces(textinfo='percent+value')
+    return fig
 
 
 def offers_amt_poland() -> go.Figure:
@@ -118,7 +118,7 @@ def offers_amt_poland() -> go.Figure:
     fig = px.choropleth_mapbox(df_woj_amt, geojson=pl_woj_json,
                                locations=df_woj_amt.index, featureidkey="properties.nazwa",
                                mapbox_style="carto-darkmatter", color='ilosc',
-                               center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85)
+                               center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85, labels={'ilosc': "Ilość Ofert"})
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
                       paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656')
     return fig
@@ -134,7 +134,7 @@ def offers_salary_poland() -> go.Figure:
     fig = px.choropleth_mapbox(df_woj_mean, geojson=pl_woj_json,
                                locations=df_woj_mean.index, featureidkey="properties.nazwa",
                                mapbox_style="carto-darkmatter", color='srednie_zarobki',
-                               center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85)
+                               center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85, labels={'srednie_zarobki': "Średnie Zarobki Brutto"})
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
                       paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656')
     return fig
