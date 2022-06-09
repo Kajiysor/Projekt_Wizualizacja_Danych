890957ce464caa710fac7a35206e35401aa34214
Jonasz Gawlik <mrbarman55@gmail.com>
2022-06-10 01:43:32 +0200

    Added average world income

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import json
 import plotly.express as px
 import plotly.graph_objects as go
 import dash
+import pycountry
 from dash import dcc, html, Input, Output
 from dash.dependencies import Input, Output
 
@@ -22,7 +23,7 @@ app.layout = html.Div(children=[
         html.Nav(children=dcc.RadioItems(
             ['Porównanie Zarobków', 'Popularność Języków',
              'Oferty Zdalne', 'Ilość Ofert w Polsce',
-             'Zarobki w Polsce'],
+             'Zarobki w Polsce', 'Zarobki na Świecie'],
             'Porównanie Zarobków',
             id='graph_select'
         )
@@ -51,6 +52,8 @@ def display_graph(selected_graph):
             return offers_amt_poland()
         case 'Zarobki w Polsce':
             return offers_salary_poland()
+        case 'Zarobki na Świecie':
+            return yearly_income_world()
 
 
 def compare_income() -> go.Figure:
@@ -77,6 +80,7 @@ def compare_income() -> go.Figure:
 
     fig.update_layout(
         title_text='Porównanie średnich zarobków w danych województwach',
+        title_font_size=22,
         yaxis_title_text='Zarobki [zł]',
         bargap=0.2,
         bargroupgap=0.1,
@@ -93,7 +97,7 @@ def compare_languages_pop() -> go.Figure:
     fig = px.pie(df_jezyki, values='ilosc_ofert',
                  names="dziedzina", color_discrete_sequence=px.colors.sequential.thermal)
     fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
-                      plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Popularność Języków", title_font_color='#cab656')
+                      plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Popularność Języków", title_font_color='#cab656', title_font_size=22)
     fig.update_traces(textposition='inside')
     return fig
 
@@ -104,7 +108,8 @@ def compare_offers_remote() -> go.Figure:
     labels = ["Praca Zdalna", "Praca Stacjonarna"]
     values = [df_jezyki.sum()["zdalnieT"], df_jezyki.sum()["zdalnieN"]]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, marker_colors=['#fde65a', '#551ebc'])], layout=go.Layout(
-        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Ilość Ofert Pracy Zdalnej a Stacjonarnej", title_font_color='#cab656'))
+        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Ilość Ofert Pracy Zdalnej a Stacjonarnej", title_font_color='#cab656',
+        title_font_size=22))
     fig.update_traces(textinfo='percent+value')
     return fig
 
@@ -120,7 +125,7 @@ def offers_amt_poland() -> go.Figure:
                                mapbox_style="carto-darkmatter", color='ilosc',
                                center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85, labels={'ilosc': "Ilość Ofert"})
     fig.update_layout(
-        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Ilość Ofert w Polsce", title_font_color='#cab656')
+        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Ilość Ofert w Polsce", title_font_color='#cab656', title_font_size=22)
     return fig
 
 
@@ -136,7 +141,26 @@ def offers_salary_poland() -> go.Figure:
                                mapbox_style="carto-darkmatter", color='srednie_zarobki',
                                center={"lat": 52.2248, "lon": 19.3120}, zoom=4.85, labels={'srednie_zarobki': "Średnie Zarobki Brutto"})
     fig.update_layout(
-        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Zarobki w Polsce", title_font_color='#cab656')
+        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Zarobki w Polsce", title_font_color='#cab656', title_font_size=22)
+    return fig
+
+
+def yearly_income_world() -> go.Figure:
+    countries = pd.DataFrame([[country.alpha_3, country.name]
+                             for country in pycountry.countries], columns=['alpha_3', 'name'])
+
+    average_countries_usd = pd.read_csv(
+        "../data/countries_salaries/average_countries_usd.csv")
+    average_countries_usd.set_index("name", inplace=True)
+
+    df_countries_income = pd.merge(
+        countries, average_countries_usd, left_on="name", right_index=True)
+    fig = px.choropleth(df_countries_income, locations="alpha_3", color='salary', template='plotly_dark',
+                        color_continuous_scale=px.colors.sequential.Plasma, labels={'salary': "Średnie Roczne Zarobki [USD]"}, hover_name='name')
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', font_color='#cab656', title_text="Zarobki na Świecie", title_font_color='#cab656',
+        title_font_size=22, margin={"r": 0, "t": 40, "l": 0, "b": 0})
+
     return fig
 
 
